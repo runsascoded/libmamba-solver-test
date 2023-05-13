6c894f9280f2f143e3fd0e0219bc3108f9ada15f
Ryan Williams <ryan@runsascoded.com>
2023-05-13 14:17:10 -0400

    build.sh env-update

--- build.sh
+++ build.sh
@@ -4,28 +4,39 @@ set -e
 
 usage() {
     echo "Usage: $0 <install-conda|install-mamba-cli|install-mamba-solver>" >&2
+    echo "Usage: $0 env-update <cli|solver>" >&2
     exit 1
 }
 
-if [ $# -ne 1 ]; then
+if [ $# -eq 0 ]; then
     usage
 fi
 IMG="$1"; shift
 
 args=()
-
 case "$IMG" in
     install-mamba-cli) BIN=mamba ;;
     install-mamba-solver) BIN=conda ;;
     install-conda) ;;
+    env-update)
+        if [ $# -ne 1 ]; then
+            usage
+        fi
+        t="$1"; shift
+        case "$t" in
+            cli) CLI=mamba ;;
+            solver) CLI=conda ;;
+            *) usage ;;
+        esac
+        tag="mamba-$t"
+        FROM="install-$tag"
+        args=(--build-arg "FROM=$FROM" --build-arg "CLI=$CLI")
+        ;;
     *) usage ;;
 esac
 
-set -x
-docker build -t$IMG -f $IMG.dockerfile .
-
-if [ "$IMG" == install-mamba-cli ]; then
-    time docker build -t$env-update:mamba-cli -f env-update.dockerfile --build-arg FROM=install-mamba-cli .
-elif [ "$IMG" == install-mamba-cli ]; then
-    time docker build -t$env-update:mamba-solver -f env-update.dockerfile --build-arg FROM=install-mamba-solver .
+if [ $# -gt 0 ]; then
+    usage
 fi
+
+time docker build -t$IMG -f $IMG.dockerfile "${args[@]}" .
